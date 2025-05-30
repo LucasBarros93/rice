286f131435d7623d43db8657a5acaff853255261
lucas.barros <lucas.am.barros@usp.br>
2025-05-29 23:50:57 -0300

    initial nvim

---
+++ nvim/init.lua
@@ -0,0 +1,9 @@
+-- ~/.config/nvim/init.lua
+
+vim.opt.number = true       -- mostrar número das linhas
+vim.opt.relativenumber = true -- número relativo (pra navegação rápida)
+vim.opt.mouse = 'a'         -- ativar mouse
+vim.opt.clipboard = 'unnamedplus' -- usar clipboard do sistema
+vim.opt.expandtab = true    -- converter tabs em espaços
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
